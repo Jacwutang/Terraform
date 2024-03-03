d23e1d40dc8500da879f4558a14c221a4af6be4e
admin <jack@jacks-mbp.lan>
2024-03-02 21:38:47 -0800

    provision ec2 with hello world

--- hello-world/main.tf
+++ hello-world/main.tf
@@ -1,1 +1,24 @@
-# Hello World TF
+# Hello World
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+}
+
+# Configure provider
+provider aws {
+    region = "us-west-2"
+}
+
+# Create EC2
+resource "aws_instance" "hello-world" {
+    ami = "ami-08d70e59c07c61a3a"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = var.instance_name
+    }
+}
