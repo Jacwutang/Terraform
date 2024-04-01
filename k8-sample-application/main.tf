514a68d64198d839df6bda0c37732711f8ab7e75
jack <jack@jacks-air.lan>
2024-04-01 14:56:12 -0700

    barebones eks cluster deployment

--- k8-sample-application/main.tf
+++ k8-sample-application/main.tf
@@ -5,10 +5,10 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
+  name = "k8-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b"]
+  azs             = ["us-east-1c", "us-east-1d"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   
   tags = {
@@ -27,7 +27,7 @@ output "vpc_private_subnets" {
 
 
 module "eks" {
-    source  = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
   cluster_name    = "my-cluster"
@@ -36,35 +36,35 @@ module "eks" {
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access  = true
 
-#   cluster_addons = {
-#     coredns = {
-#       most_recent = true
-#     }
-#     kube-proxy = {
-#       most_recent = true
-#     }
-#     vpc-cni = {
-#       most_recent = true
-#     }
-#   }
+  cluster_addons = {
+    # coredns = {
+    #   most_recent = true
+    # }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
 
   vpc_id = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  # EKS Managed Node Group(s)
-#   eks_managed_node_group_defaults = {
-#     instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
-#   }
+  #EKS Managed Node Group(s)
+  # eks_managed_node_group_defaults = {
+  #   instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
+  # }
 
-#   eks_managed_node_groups = {
-#     example = {
-#       min_size     = 1
-#       max_size     = 1
-#       desired_size = 1
+  # eks_managed_node_groups = {
+  #   example = {
+  #     min_size     = 1
+  #     max_size     = 1
+  #     desired_size = 1
 
-#       instance_types = ["t3.large"]
-#       capacity_type  = "SPOT"
-#     }
-#   }
+  #     instance_types = ["t3.large"]
+  #     capacity_type  = "SPOT"
+  #   }
+  # }
 }
 
